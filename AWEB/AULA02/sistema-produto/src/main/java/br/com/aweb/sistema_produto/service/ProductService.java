9146020f98dced57b072e53da4cf3fc7dff67115
MykyBalb <mykaelbalbino06@gmail.com>
2025-08-22 22:51:49 -0300

    aulao

---
+++ AWEB/AULA02/sistema-produto/src/main/java/br/com/aweb/sistema_produto/service/ProductService.java
@@ -0,0 +1,44 @@
+package br.com.aweb.sistema_produto.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.aweb.sistema_produto.model.Product;
+import br.com.aweb.sistema_produto.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    //Listar todos os produtos
+    public List<Product> allProducts(){
+        return productRepository.findAll();
+    }
+
+    //Buscar o produto por id
+    public Product findProduct(Long id){
+        Optional<Product> pd = productRepository.findById(id);
+        if(pd.isPresent()){
+            return pd.get();
+        }
+        throw new RuntimeException("Produto não encontrado!!");  
+    }
+
+    //Cadastrar || alterar produto
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    //Remover produto
+    public void deleteProduct(Long id){
+        if(!productRepository.existsById(id)){
+            throw new RuntimeException("Erro ao deletar produto!!");
+        }
+        productRepository.deleteById(id);
+    }
+}
---
+++ AWEB/AULA02/sistema-produto/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (name,price,description) VALUES 
+    ('Notebook',3500.00,'Notebook 16GB RAM, GTX2070 SUPER'),
+    ('Smartphone',1999.99,'Tela 6.15, 128GB'),
+    ('Tablet',1200.50,'Tela 10, 64GB'),
+    ('Monitor',899.99,'24 Pol Full HD 144HZ'),
+    ('Teclado',199.00,'Teclado mecânico, switch blue, RGB');
